d5859c2033423671adfbdd8e8df492d77f2983be
MSI\jeang <jeangodecoster@gmail.com>
2016-10-24 16:56:45 +0200

    Cours 5 commit final

---
+++ Assets/Scripts/DieBehaviours/ScoreDieBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreDieBehaviour : AbstractDieBehaviour {
+
+	public int score;
+
+	#region implemented abstract members of AbstractDieBehaviour
+	public override void Die (GameObject deadObject)
+	{
+		ScoreManager.GetInstance ().AddScore (score);
+	}
+	#endregion
+}
---
+++ Assets/Scripts/Score/ScoreTracker.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreTracker : MonoBehaviour {
+
+	public Text scoreText;
+	public Text hiScoreText;
+
+	ScoreManager scoreMan;
+
+	void Awake() {
+		scoreMan = ScoreManager.GetInstance ();
+		UpdateScore (0);
+		UpdateHiScore (0);
+	}
+
+	void OnEnable() {
+		scoreMan.OnScore += UpdateScore;
+		scoreMan.OnHiScore += UpdateHiScore;
+	}
+	void OnDisable() {
+		scoreMan.OnScore -= UpdateScore;
+		scoreMan.OnHiScore -= UpdateHiScore;
+	}
+
+	private void UpdateScore(int scoreDiff) {
+		scoreText.text = "" + scoreMan.GetScore ();
+	}
+
+	private void UpdateHiScore(int scoreDiff) {
+		hiScoreText.text = "" + scoreMan.GetHiScore ();
+	}
+
+
+
+}
